4b086b7d6b1c5b110323db5c0eba4392744fe03f
1soja <90239033+1soja@users.noreply.github.com>
2023-01-25 13:56:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "917306004019, 917306004019";
 }
 
 global.owner = gg.split(",");
